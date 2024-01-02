fd84042021f8fbffcaa3d3f6529382e1b738d98f
sainthm <sainthm@naver.com>
2024-01-03 08:42:05 +0900

    refactor: 문자열 포맷팅 방식별 테스트
    세 가지 방식의 문자열 포맷팅 테스트

--- python/do-it-basic/example/tree.py
+++ python/do-it-basic/example/tree.py
@@ -3,6 +3,8 @@
 treeHit = 0
 while treeHit < 10:
     treeHit += 1
-    print("나무를 %d번 찍었습니다." % treeHit)
+    # print("나무를 %d번 찍었습니다." % treeHit)
+    # print("나무를 {0}번 찍었습니다.".format(treeHit))
+    print(f"나무를 {treeHit}번 찍었습니다.")
     if treeHit == 10:
         print("나무 넘어갑니다!")
