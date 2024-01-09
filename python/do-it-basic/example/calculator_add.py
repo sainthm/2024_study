ea35c5e5d3e3e2b40136b075b4752c6ba1721fdd
sainthm <sainthm@naver.com>
2024-01-09 22:16:20 +0900

    Edit calculator_add.py example

--- python/do-it-basic/example/calculator_add.py
+++ python/do-it-basic/example/calculator_add.py
@@ -1,11 +1,29 @@
-# calculator_add.py
+# # calculator_add.py
 
-result = 0
+# result = 0
 
-def add(num):
-    global result
-    result += num
-    return result
+# def add(num):
+#     global result
+#     result += num
+#     return result
 
-print(add(3))
-print(add(4))
+# print(add(3))
+# print(add(4))
+
+result1 = 0
+result2 = 0
+
+def add1(num):
+    global result1
+    result1 += num
+    return result1
+
+def add2(num):
+    global result2
+    result2 += num
+    return result2
+
+print(add1(3))
+print(add1(4))
+print(add2(3))
+print(add2(7))
