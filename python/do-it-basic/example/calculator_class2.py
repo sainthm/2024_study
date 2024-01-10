9a8a6c4e67580fa1c9bc25d322ac88fb9f412a18
sainthm <sainthm@naver.com>
2024-01-10 23:35:32 +0900

    Add calculator_class2.py example

---
+++ python/do-it-basic/example/calculator_class2.py
@@ -0,0 +1,18 @@
+# calculator_class2.py
+
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
