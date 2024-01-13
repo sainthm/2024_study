be7577000a1ad7b8065fc4f96848dd6091a300c6
sainthm <sainthm@naver.com>
2024-01-13 15:35:43 +0900

    Edit xx_times.py example

--- python/do-it-basic/example/two_times.py
+++ python/do-it-basic/example/two_times.py
@@ -1,5 +1,6 @@
-# two_times.py
+# xx_times.py
 
+## two_times
 def two_times(numberList):
     result = []
     for number in numberList:
@@ -9,8 +10,13 @@ def two_times(numberList):
 result = two_times([1, 2, 3, 4])
 print(result)
 
+## 3
 def three_times(x): 
     return x * 3
 
 print(list(map(three_times, [1, 2, 3, 4])))
 # print(map(three_times, [1, 2, 3, 4]))
+
+
+## 4
+print(list(map(lambda a: a*4, [1, 2, 3, 4])))
