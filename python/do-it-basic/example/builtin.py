17f2c3085e644eb88b6ad9223007230bd892200f
sainthm <sainthm@naver.com>
2024-01-13 15:07:12 +0900

    Edit builtin.py example

--- python/do-it-basic/example/builtin.py
+++ python/do-it-basic/example/builtin.py
@@ -1,5 +1,6 @@
 # builtin.py
 
+print("dir")
 dir([1, 2, 3])
 print(dir([1, 2, 3]))
 
@@ -7,3 +8,9 @@ print("*" * 10)
 
 dir({"1": "a"})
 print(dir({"1": "a"}))
+
+print("*" * 10)
+
+print("enumerate")
+for i, name in enumerate(["body", "foo", "bar"]):
+    print(i, name)
