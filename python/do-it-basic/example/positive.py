5f2b8f47e8a0191599ead1b828a72f9bddb81d13
sainthm <sainthm@naver.com>
2024-01-13 15:24:47 +0900

    Edit comment of positive.py example

--- python/do-it-basic/example/positive.py
+++ python/do-it-basic/example/positive.py
@@ -10,7 +10,7 @@ def positive(l):
 print(positive([1, -3, 2, 0, -5, 6]))
 
 ####################
-# filter(name_of_function, replicable_data_type)
+# filter(name_of_function, iterable_data_type)
 ####################
 def positive(x):
     return x > 0
