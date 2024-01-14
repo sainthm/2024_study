853d19b0d70505b18f1afdf0cb916d99907c68c1
sainthm <sainthm@naver.com>
2024-01-14 23:40:59 +0900

    Edit memo.py example

--- python/do-it-basic/example/memo.py
+++ python/do-it-basic/example/memo.py
@@ -13,4 +13,9 @@ if option == '-a':
     f = open('memo.txt', 'a')
     f.write(memo)
     f.write('\n')
-    f.close
+    f.close()
+elif option == '-v':
+    f = open('memo.txt')
+    memo = f.read()
+    f.close()
+    print(memo)
