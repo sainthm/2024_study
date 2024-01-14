7063393e90449e6155210b56d94b104d30fbe8ea
sainthm <sainthm@naver.com>
2024-01-14 21:57:57 +0900

    Add get_total_page.py example

---
+++ python/do-it-basic/example/get_total_page.py
@@ -0,0 +1,9 @@
+# get_total_page.py
+
+def getTotalPage(m, n):
+    return m // n + 1
+
+print(getTotalPage(5, 10))
+print(getTotalPage(15, 10))
+print(getTotalPage(25, 10))
+print(getTotalPage(35, 10))
