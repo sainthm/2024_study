807cea05f2e352083059c488c4c42cbcc1da216c
sainthm <sainthm@naver.com>
2024-01-14 21:59:23 +0900

    Edit get_total_page.py example

--- python/do-it-basic/example/get_total_page.py
+++ python/do-it-basic/example/get_total_page.py
@@ -1,9 +1,12 @@
 # get_total_page.py
 
 def getTotalPage(m, n):
-    return m // n + 1
+    if m % n == 0:
+        return m // n
+    else:
+        return m // n + 1
 
 print(getTotalPage(5, 10))
 print(getTotalPage(15, 10))
 print(getTotalPage(25, 10))
-print(getTotalPage(35, 10))
+print(getTotalPage(30, 10))
