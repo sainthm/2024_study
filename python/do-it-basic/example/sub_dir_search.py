2a4712ec834810d61ff0956707d70d207468ab8f
sainthm <sainthm@naver.com>
2024-01-15 20:52:22 +0900

    Edit sub_dir_search.py example

--- python/do-it-basic/example/sub_dir_search.py
+++ python/do-it-basic/example/sub_dir_search.py
@@ -6,6 +6,8 @@ def search(dirname):
     filenames = os.listdir(dirname)
     for filename in filenames:
         full_filename = os.path.join(dirname, filename)
-        print(full_filename)
+        ext = os.path.splitext(full_filename)[-1]
+        if ext == ".py":
+            print(full_filename)
 
 search("/Users/")
