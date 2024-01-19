fab77802677c1d58c1347d1e6f1b49cf5ddc241e
sainthm <sainthm@naver.com>
2024-01-19 22:34:02 +0900

    Edit main.tf example

--- terraform/iac-starts-with-terraform/03.start/main.tf
+++ terraform/iac-starts-with-terraform/03.start/main.tf
@@ -1,22 +1,27 @@
-terraform {
-  backend "local" {
-    path = "state/terraform.rfstate"
-  }
-  required_version = ">= 1.0.0"
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.0.0"
-    }
-  }
-}
+# terraform {
+#   backend "local" {
+#     path = "state/terraform.rfstate"
+#   }
+#   required_version = ">= 1.0.0"
+#   required_providers {
+#     local = {
+#       source  = "hashicorp/local"
+#       version = ">= 2.0.0"
+#     }
+#   }
+# }
 
 resource "local_file" "abc" {
-  content  = "123456!"
+  content  = "123!"
   filename = "${path.module}/abc.txt"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-02d081c743d676996"
-  instance_type = "t2.micro"
+resource "local_file" "def" {
+  content  = "456!"
+  filename = "${path.module}/def.txt"
 }
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-02d081c743d676996"
+#   instance_type = "t2.micro"
+# }
